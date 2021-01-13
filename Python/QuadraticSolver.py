f53f13efc9ef80b6f14e708f8df3bcb6c6a2e8cf
piper <pguiffr62@charlottesvilleschools.org>
2021-01-13 00:57:28 +0000

    adding my strings and loops python program

--- Python/QuadraticSolver.py
+++ Python/QuadraticSolver.py
@@ -1,22 +1,22 @@
-print("Quadratic Solver") 
+print("Quadratic Solver") #prints initial message
 print("Enter the coefficients for ax^2 + bx + c = 0")
 
 import math
 import cmath
-def quadratic():
-	a=int(input("enter a: "))
+def quadratic():  #quadratic function 
+	a=int(input("enter a: ")) #inputs for quadratic values
 	b=int(input("enter b: "))
 	c=int(input("enter c: "))
 	d=((math.pow(b,2))-(4*a*c)) #calculates discriminant  
 
 	if d<0:
-		print("no real roots")
+		print("no real roots") #if discriminant is < 0 prints no real roots
 	elif d==0:
 		root1 = float(-b + math.sqrt(d))/ (2 * a)
-		print("equal roots")
+		print("equal roots") #if discriminant =0 prints one root
 		print(root1)
-	elif d>0:
-		root1 = (-b + cmath.sqrt(d))/ (2 * a)
+	elif d>0:  #if d is > 0 calulates and prints both roots
+		root1 = (-b + cmath.sqrt(d))/ (2 * a) 
 		root2 = (-b - cmath.sqrt(d))/ (2 * a) 
 		print("real roots")
 		print(root1)
---
+++ Python/stringsnloops.py
@@ -0,0 +1,12 @@
+
+print("Write a simple sentence")
+
+sentence =  input(" ")
+
+print(sentence.split('-'))
+
+for letter in sentence:
+               print(letter)
+                
+
+ 
